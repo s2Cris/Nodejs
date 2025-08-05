76d1ec684103b7459d0e638602a844065500c86b
Cris <cristian.marcheti@etec.sp.gov.br>
2025-08-05 01:37:38 -0300

    node

---
+++ React Node/routes/cursos.js
@@ -0,0 +1,29 @@
+// Rotas de Categoria
+const express = require("express");
+const router = express.Router();
+
+//listar categoria
+router.get("/", async (req, res)=> {  // router - rota | "/" - rota raiz | render - montar/construir
+    res.render("base", {
+        title: "Listar Cursos",
+        view: "cursos/show",
+    })
+});
+
+//edit categoria
+router.get("/edit", async (req, res)=> {  // router - rota | "/edit" - rota atual | render - montar/construir
+    res.render("base", {
+        title: "Editar Cursos",
+        view: "cursos/edit",
+    })
+});
+
+//add categoria
+router.get("/add", async (req, res)=> {  // router - rota | "/add" - rota atual | render - montar/construir
+    res.render("base", {
+        title: "Adicionar Cursos",
+        view: "cursos/add",
+    })
+});
+
+module.exports = router;
